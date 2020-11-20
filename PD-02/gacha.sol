698d5c4cb18064adb90eca4fd339e0dd27ddb0ba
Kean Yew Lee <keanyewl@gmail.com>
2020-11-20 15:40:24 +0100

    pd changes

--- PD-02/gacha.sol
+++ PD-02/gacha.sol
@@ -3,32 +3,31 @@ pragma solidity >=0.7.0;
 
 contract Game {
     uint _pull;
-    string _Banner;
+   
     uint[] public pulls ;
     uint Fivestar;
     uint Fourstar;
     uint Threestar;
 
-    constructor(uint pull, string memory Banner) public {   
-        require(pull != 1 || pull != 10, "amount of pulls not valid");
-        _pull = pull;
+    function Start(uint pull) public {   
+       if (pull == 1 || pull == 10){
+           _pull = pull;
+           PullCharacter();
+       }
     }
 
-    function randompull(uint i) private returns (uint) {
-    uint randomnumber = uint(keccak256(abi.encodePacked(block.timestamp , msg.sender, i))) % 200;
-    return randomnumber;
+    function Randompull(uint i) public returns (uint) {
+        uint randomnumber = uint(keccak256(abi.encodePacked(block.timestamp , msg.sender, i))) % 200;
+        return randomnumber;
     }
 
     function PullCharacter() public {
         for(uint i =0; i< _pull; i++){
-        pulls.push(randompull(i));
+        pulls.push(Randompull(i));
         }
     }
     
     function characterLoop() public {
-        
-        // WARN: This unbounded for loop is an anti-pattern
-        
         for (uint i = 0; i<pulls.length; i++) {
            if( pulls[i] == 0){
                Fivestar++;
