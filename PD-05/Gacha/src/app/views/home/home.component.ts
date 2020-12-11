68641f1d507e732ac088d80c9f860048bc1a5581
Kean Yew Lee <keanyewl@gmail.com>
2020-12-11 17:03:28 +0100

    pd 5

--- PD-05/Gacha/src/app/views/home/home.component.ts
+++ PD-05/Gacha/src/app/views/home/home.component.ts
@@ -12,78 +12,92 @@ export class HomeComponent implements OnInit {
   gacha: any;
   error: string;
   option: number;
+  result: any;
+  contract = "0x9E05615Af5E8E6A661DA53caaCefb9e5aF1F8688";
+  account: any;
   constructor() {}
 
   ngOnInit():void {
     this.option = 0;
-    const contract = "0x6F7DbC6622FF695Eda25022FF0a6Ba05cf55f39D";
-    let web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
-    this.gacha = new web3.eth.Contract(
-      [
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "pull",
-              "type": "uint256"
-            }
-          ],
-          "name": "Start",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": false,
-              "internalType": "string",
-              "name": "CharName",
-              "type": "string"
-            },
-            {
-              "indexed": false,
-              "internalType": "uint8",
-              "name": "Rating",
-              "type": "uint8"
-            }
-          ],
-          "name": "WantGachaPull",
-          "type": "event"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "name": "PullResult",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "naam",
-              "type": "string"
-            },
-            {
-              "internalType": "uint8",
-              "name": "Rating",
-              "type": "uint8"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        }
-      ] , contract);
-
-    console.log(this.gacha);
+    this.asyncloaded();
   }
+    async asyncloaded() {
+    var web3 = new Web3(Web3.givenProvider); // provider from metamask
+    var result = await web3.eth.requestAccounts().catch(x => console.log(x.message));
+    console.log(web3.version); // note: use  (back quote)
+    const network = await web3.eth.net.getId().catch(reason => console.log(reason));
+    if (typeof network === 'undefined' || network != 4) { console.log("Please select Rinkeby test network"); return; }
+    console.log("Ethereum network: Rinkeby")
+    var accounts = await web3.eth.getAccounts();
+    this.account = accounts[0];
+    console.log(accounts[0]); // show current user.
+    this.gacha = new web3.eth.Contract( [
+      {
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "pull",
+            "type": "uint256"
+          }
+        ],
+        "name": "Start",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": false,
+            "internalType": "string",
+            "name": "CharName",
+            "type": "string"
+          },
+          {
+            "indexed": false,
+            "internalType": "uint8",
+            "name": "Rating",
+            "type": "uint8"
+          }
+        ],
+        "name": "WantGachaPull",
+        "type": "event"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "name": "PullResult",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "naam",
+            "type": "string"
+          },
+          {
+            "internalType": "uint8",
+            "name": "Rating",
+            "type": "uint8"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ], this.contract);    
+    console.log(this.gacha);
+}
 
   Start(amount:number) {
-    this.gacha.methods.Start(amount).send({from:"0xf83e9122C35Db685495B6b94ab4ddD51a55c4020"}).then((res: any) => {
+    this.gacha.methods.Start(amount).send({from:this.account}).then((res: any) => {
       console.log(res);
+      if (amount == 1){this.result = [res.events.WantGachaPull];}
+      if (amount == 10){this.result = res.events.WantGachaPull;}
+      console.log(this.result)
     }).catch((err: any) => {
       console.error(err);
     });
