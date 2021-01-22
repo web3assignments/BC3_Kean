8608a7b1a460167a19e256f99cfb12e71584c9f8
keanYl <38686996+keanYl@users.noreply.github.com>
2021-01-22 17:28:29 +0100

    Create pd-13.sol

---
+++ PD-13/pd-13.sol
@@ -0,0 +1,49 @@
+pragma solidity >= 0.6;
+pragma experimental ABIEncoderV2;
+import "remix_tests.sol"; // this import is automatically injected by Remix.
+import "gachav.sol";
+
+// File name has to end with '_test.sol', this file can contain more than one testSuite contracts
+contract GachaTest {
+    Game gacha;
+ 
+     struct Unit{
+        string naam;
+        uint8 Rating;
+    }
+    
+    /// 'beforeAll' runs before all other tests
+    /// More special functions are: 'beforeEach', 'beforeAll', 'afterEach' & 'afterAll'
+    function beforeAll() public {
+        // Here should instantiate tested contract
+        gacha = new Game();
+    }
+     function testOnepull()  public  {
+        gacha.Start(1);
+        uint result =  gacha.getPullResultList().length;
+        Assert.equal(result,1,"pulled 1 time");
+    }
+      
+    function testTenPull()  public  {
+        gacha.Start(10);
+        uint result =  gacha.getPullResultList().length;
+        Assert.equal(result,10,"pulled 10 times");
+    }
+     
+    function testFivestar() public{
+         gacha.setCharachter(0);
+         uint result1 =  uint8(gacha.getPullLastResult());
+        Assert.equal(result1, 5, "pulled 5 star");
+    }
+     function testFourstar() public{
+         gacha.setCharachter(6);
+         uint result1 =  uint8(gacha.getPullLastResult());
+        Assert.equal(result1, 4, "pulled 4 star");
+    }
+     function testThreestar() public{
+         gacha.setCharachter(15);
+         uint result1 =  uint8(gacha.getPullLastResult());
+        Assert.equal(result1, 3, "pulled 3 star");
+    }
+    
+}
